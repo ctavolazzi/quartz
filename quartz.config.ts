3db5efa17a2d3e13d9a358102082cb50d849b64f
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-11-14 09:43:15 -0800

    Add Template and Mobile ALOHA example file

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 TheCoffeeJesus Garden",
+    pageTitle: "🪴",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
