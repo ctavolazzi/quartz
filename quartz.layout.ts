d4212da7b6cbe4f05839b2ba5db1847656a9493e
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-11-16 14:26:33 -0800

    Copy Text to Clipboard? Yes please

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,9 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.CopyButton(),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -23,7 +21,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.CopyButton(),
   ],
   left: [
     Component.PageTitle(),
