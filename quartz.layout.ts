5ba37e78b592823f33fe7716bde8970454028a4a
Christopher Tavolazzi <ctavolazzi@gmail.com>
2024-11-16 14:28:40 -0800

    Copy Text to Clipboard? Yes please

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,6 +21,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.CopyButton(),
   ],
   left: [
     Component.PageTitle(),
